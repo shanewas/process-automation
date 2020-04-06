023c8fc1cc0815de613f2c81be36cbbf4178f6d1
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-04-06 16:55:28 +0600

    path solved || locally saving & retrieving the data in JSON

--- js/dataControl/botlist.js
+++ js/dataControl/botlist.js
@@ -1,37 +1,40 @@
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 
 // Add Bot Function
 const addBot = function (botName, runTime, category) {
-    // fetching stored bots from json file
-    const bots = loadBots();
+	// fetching stored bots from json file
+	const bots = loadBots();
 
-    bots.push({
-        "botName": botName,
-        "runTime": runTime,
-        "category":category
-    });
+	bots.push({
+		botName: botName,
+		runTime: runTime,
+		category: category,
+	});
 
-    saveBots(bots);
+	saveBots(bots);
 };
 
 // Fetch BotList Function
 const loadBots = function () {
-    try {
-        const dataBuffer = fs.readFileSync('botlist.json');
-        const dataJSON = dataBuffer.toString();
-        return JSON.parse(dataJSON);
-    }
-    catch { 
-        return [];
-    }
+	try {
+		const dataBuffer = fs.readFileSync(`${path.join(__dirname, "botlist.json")}`);
+		const dataJSON = dataBuffer.toString();
+		return JSON.parse(dataJSON);
+	} catch {
+		return [];
+	}
 };
 
 // Store BotList Function
 const saveBots = function (bots) {
-    const dataJSON = JSON.stringify(bots);
-    fs.writeFileSync('botlist.json', dataJSON);
-}
+	const dataJSON = JSON.stringify(bots);
+	fs.writeFileSync(`${path.join(__dirname, "botlist.json")}`, dataJSON);
+};
 
 module.exports = {
-    addBot: addBot
+	addBot: addBot,
 };
+
+addBot("potato", 1, "potao");
+console.log("asdadsf");
