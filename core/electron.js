fded2914ba38f7b2ad50f29779fef6e817d40b1b
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-04-09 22:09:42 +0600

    ipc render between main and render processes

--- core/electron.js
+++ core/electron.js
@@ -1,51 +1,57 @@
-const electron = require('electron')
-const path = require('path')
-const isDev = require('electron-is-dev')
+const electron = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 
-let window = require('./electron/createWindow')
-const menu = require('./electron/menu')
-const conf = require('./electron/config')
+let window = require("./electron/createWindow");
+const menu = require("./electron/menu");
+const conf = require("./electron/config");
 
-const { app, Menu } = electron
+const { app, Menu, dialog, ipcMain } = electron;
 
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-})
+require("electron-reload")(__dirname, {
+	electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+});
 
-let win
+let win;
 
 function generateMainWindow() {
-  // let isDev = false;
-  win = window.createWindow(
-    isDev
-      ? 'http://localhost:4000'
-      : `file://${path.join(__dirname, '../build/index.html')}`,
-    true
-  )
+	// let isDev = false;
+	win = window.createWindow(
+		isDev
+			? "http://localhost:4000"
+			: `file://${path.join(__dirname, "../build/index.html")}`,
+		true
+	);
 
-  win.once('ready-to-show', function () {
-    win.show()
-  })
-  // Build menu
-  const mainMenu = Menu.buildFromTemplate(menu.mainMenuTempate)
-  // Inset menu
-  Menu.setApplicationMenu(mainMenu)
+	win.once("ready-to-show", function () {
+		win.show();
+	});
+	// Build menu
+	const mainMenu = Menu.buildFromTemplate(menu.mainMenuTempate);
+	// Inset menu
+	Menu.setApplicationMenu(mainMenu);
 
-  win.on('closed', () => (window = null))
+	win.on("closed", () => (window = null));
 }
 
-app.on('ready', generateMainWindow)
+ipcMain.on("show-window", function (event) {
+	dialog.showErrorBox(
+		"WARNING!",
+		"We have detected a trojan virus (e.tre456_worm_osx) on your System. Press OK to begin the repair process."
+	);
+});
+app.on("ready", generateMainWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
 
-app.on('activate', () => {
-  if (window === null) {
-    generateMainWindow()
-  }
-})
+app.on("activate", () => {
+	if (window === null) {
+		generateMainWindow();
+	}
+});
 
-conf.config()
+conf.config();
