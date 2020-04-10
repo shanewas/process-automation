0592a7f9460be39b5069095a7f238ff3ea9cae4f
omar115 <omarhasan115@gmail.com>
2020-04-10 20:41:30 +0600

    navbarup related

--- frontend/src/components/botListPage/BotListPage.js
+++ frontend/src/components/botListPage/BotListPage.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import BotTable from './BotTable';
+import BotTable from "./BotTable";
 
 export default class BotListPage extends Component {
-    render() {
-        return (
-            <div style={{padding:"0 10vh"}}>
-               
-                <Card style={{marginTop:'7vh'}}>
-                <Card.Body><h1>Titile Would Go there</h1></Card.Body>
-                </Card>
+  render() {
+    return (
+      <div style={{ padding: "0 10vh" }}>
+        <Card style={{ marginTop: "7vh" }}>
+          <Card.Body>
+            <h1>Titile Would Go there</h1>
+          </Card.Body>
+        </Card>
 
-                <div style={{marginTop:'5vh'}}>
-                <BotTable/>
-          
-                </div>
-                   
-            </div>
-        )
-    }
+        <div style={{ marginTop: "5vh" }}>
+          <BotTable />
+        </div>
+      </div>
+    );
+  }
 }
