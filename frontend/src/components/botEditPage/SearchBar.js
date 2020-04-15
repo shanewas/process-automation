01d59f7f9256d2c999b7202ac200e3b07e31ad09
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-04-15 20:40:07 +0600

    test method runnning on renderer to check recieving data using the receive method || works

--- frontend/src/components/botEditPage/SearchBar.js
+++ frontend/src/components/botEditPage/SearchBar.js
@@ -1,42 +1,43 @@
 import React, { Component } from "react";
-import * as electron from "../../electronScript"
-
+import * as electron from "../../electronScript";
 
 export default class SearchBar extends Component {
+	state = {
+		link: "",
+	};
 
-  state = {
-    link: "",
-  };
+	handleChange = (e) => {
+		this.setState({
+			link: e.target.value,
+		});
+	};
 
-  handleChange = (e) => {
-    this.setState({
-      link: e.target.value,
-    });
-  };
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("form submitted", this.state);
+		electron.send(electron.SearchLinkChannel, this.state.link);
+	};
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("form submitted", this.state);
-    electron.send(electron.SearchLinkChannel,this.state.link);
-  };
- 
+	test = (e) => {
+		electron.receive(electron.SearchLinkChannel);
+	};
 
-  render() {
-    return (
-      <div>
-
-          <form onSubmit={this.handleSubmit} className="searchBox">
-          <input
-            onChange={this.handleChange}
-            className="searchInput"
-            type="text"
-            placeholder="Search..."
-          />
-          <button className="searchButton">
-            <i className="large material-icons">search</i>
-          </button>
-          </form>
-        </div>
-    );
-  }
+	render() {
+		this.test();
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit} className='searchBox'>
+					<input
+						onChange={this.handleChange}
+						className='searchInput'
+						type='text'
+						placeholder='Search...'
+					/>
+					<button className='searchButton'>
+						<i className='large material-icons'>search</i>
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
