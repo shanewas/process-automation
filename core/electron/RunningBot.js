16700ef75fdd0d5d3935feb5aef22bbf8c6d1c5b
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-05-06 17:11:56 +0600

    filling data from csv || ipc issue re-rendering solve || botlist csv return promise fixed || ready state check on click

--- core/electron/RunningBot.js
+++ core/electron/RunningBot.js
@@ -1,33 +1,35 @@
 const electron = require("electron");
 
 const { ipcRenderer, remote } = electron;
-
-document.addEventListener("readystatechange", (event) => {
+async function processRunner(event) {
+	ipcRenderer.send("need-process");
+}
+ipcRenderer.on("form-fill-up", async function (e, package) {
+	xPathRes = document.evaluate(
+		package.path,
+		document,
+		null,
+		XPathResult.FIRST_ORDERED_NODE_TYPE,
+		null
+	);
+	xPathRes.singleNodeValue.value = package.dat;
+	processRunner(event);
+});
+ipcRenderer.on("click-it", async function (e, path) {
+	xPathRes = document.evaluate(
+		path,
+		document,
+		null,
+		XPathResult.FIRST_ORDERED_NODE_TYPE,
+		null
+	);
+	xPathRes.singleNodeValue.click();
+	if (document.readyState !== "complete") {
+		processRunner(event);
+	}
+});
+document.addEventListener("readystatechange", async (event) => {
 	if (event.target.readyState === "complete") {
-		console.log("loaded");
-		ipcRenderer.send("need-form");
-		ipcRenderer.on("form-fill-up", function (e, package) {
-			xPathRes = document.evaluate(
-				package.xpath,
-				document,
-				null,
-				XPathResult.FIRST_ORDERED_NODE_TYPE,
-				null
-			);
-			xPathRes.singleNodeValue.value = package.loadData;
-		});
-		// remote.ipcMain.removeAllListeners("form-fill-up");
-		ipcRenderer.send("need-click");
-		ipcRenderer.on("click-it", function (e, package) {
-			xPathRes = document.evaluate(
-				package.xpath,
-				document,
-				null,
-				XPathResult.FIRST_ORDERED_NODE_TYPE,
-				null
-			);
-			xPathRes.singleNodeValue.click();
-		});
-		// remote.ipcMain.removeAllListeners("click-it");
+		await processRunner(event);
 	}
 });
