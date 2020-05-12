329197b0a17919fa31003189149837d1817d9bb3
Shanewas Ahmed Nabil <shanewasahmed@gmail.com>
2020-05-12 14:14:32 +0600

    highlighting DOM element before selecting for xpath || need more css tricks || not complete

--- core/electron/preload.js
+++ core/electron/preload.js
@@ -2,6 +2,47 @@ const xpath = require("../modules/xpath");
 const { ipcSend } = require("./ipcManage");
 
 window.onload = function () {
+	var css = document.createElement("style");
+	css.type = "text/css";
+
+	var styles = `
+		.highlight { 	background-color: #3FE0D0; 
+						// border-style: solid;
+						border-color: coral; 
+					}`;
+
+	if (css.styleSheet) css.styleSheet.cssText = styles;
+	else css.appendChild(document.createTextNode(styles));
+
+	document.getElementsByTagName("head")[0].appendChild(css);
+	(function () {
+		var prev;
+
+		if (document.body.addEventListener) {
+			document.body.addEventListener("mouseover", handler, false);
+		} else if (document.body.attachEvent) {
+			document.body.attachEvent("mouseover", function (e) {
+				return handler(e || window.event);
+			});
+		} else {
+			document.body.onmouseover = handler;
+		}
+
+		function handler(event) {
+			if (event.target === document.body || (prev && prev === event.target)) {
+				return;
+			}
+			if (prev) {
+				prev.className = prev.className.replace(/\bhighlight\b/, "");
+				prev = undefined;
+			}
+			if (event.target) {
+				prev = event.target;
+				prev.className += " highlight";
+			}
+		}
+	})();
+
 	const body = document.querySelector("body");
 	body.addEventListener("click", (e) => {
 		if (e.shiftKey) {
