f74ee6e7e6e85e80595243eb61be95c7655ea321
Rashedul Kabir <rkabir.rashed@gmail.com>
2020-06-08 12:21:51 +0600

    changes for linux build

--- backend/dataControl/botlist.js
+++ backend/dataControl/botlist.js
@@ -9,9 +9,9 @@ const dbFactory = (fileName) =>
 	DataStore.create({
 		filename: isDev
 			? path.join("./backend/data/", fileName)
-			// : path.join("./backend/data/", fileName), //LINUX BUILD TILL SPRINT 2 TODO: Figure out how to handle this
+			: path.join("./backend/data/", fileName), //LINUX BUILD TILL SPRINT 2 TODO: Figure out how to handle this
 			// : path.join("./backend/data/", fileName).replace('/app.asar', ''), //LINUX BUILD TILL SPRINT 2 TODO: Figure out how to handle this
-			: path.join(app.getAppPath("userData"), "../backend/data/", fileName), // WINDOWS BUILD
+			// : path.join(app.getAppPath("userData"), "../backend/data/", fileName), // WINDOWS BUILD
 			// : path.join(__dirname, "../data/", fileName).replace('/app.asar', ''),
 		timestampData: true,
 		autoload: true,
