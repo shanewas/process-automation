52ed17fee6721bf978cf71cb5c529321aba98df3
Depender Sethi <rudranet.com@gmail.com>
2020-06-23 13:00:07 +0530

    removed ocr default val from reducer

--- frontend/src/Store/rootReducer.js
+++ frontend/src/Store/rootReducer.js
@@ -94,7 +94,7 @@ const UnselectHeader = (state, index) => {
 };
 const EntryProcess = (state, process) => {
   const tProcess = { ...process };
-  if (tProcess.tagName === "ScreenShot") tProcess.ocr = false;
+  // if (tProcess.tagName === "ScreenShot") tProcess.ocr = false;
   const newprocess = [...state.process, tProcess];
   return {
     ...state,
