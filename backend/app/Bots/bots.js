cc16b88ed7122f812a3360bfb6f89fa95e25c35c
Nadib <h.nadib@ferntechsolutions.com>
2020-07-23 15:14:50 +0600

    saparated external conectivity

--- backend/app/Bots/bots.js
+++ backend/app/Bots/bots.js
@@ -1,36 +1,39 @@
 const { ipcMain } = require("electron");
 const botlist = require("../../controller/local/local");
+function botHandler() {
+  ipcMain.handle("bot-name", async (event, botName) => {
+    const result = await botlist.fetchBot(botName);
+    return result;
+  });
 
-ipcMain.handle("bot-name", async (event, botName) => {
-	const result = await botlist.fetchBot(botName);
-	return result;
-});
+  ipcMain.handle("add-bot", async (event, botName, botType) => {
+    const result = await botlist.addBot(botName, botType);
+    return result;
+  });
 
-ipcMain.handle("add-bot", async (event, botName, botType) => {
-	const result = await botlist.addBot(botName, botType);
-	return result;
-});
+  ipcMain.on("remove-bot", async (event, botName) => {
+    await botlist.removeBot(botName);
+  });
 
-ipcMain.on("remove-bot", async (event, botName) => {
-	await botlist.removeBot(botName);
-});
+  ipcMain.on("update-bot-process", async (event, botName, BOTPROCESS) => {
+    await botlist.updateBotProcess(botName, BOTPROCESS);
+  });
 
-ipcMain.on("update-bot-process", async (event, botName, BOTPROCESS) => {
-	await botlist.updateBotProcess(botName, BOTPROCESS);
-});
+  ipcMain.on("update-bot", async (event, botName, saveBotObj) => {
+    await botlist.editBot(
+      botName,
+      saveBotObj.filepath,
+      saveBotObj.headers,
+      saveBotObj.status,
+      saveBotObj.botIteration,
+      saveBotObj.variables
+    );
+  });
 
-ipcMain.on("update-bot", async (event, botName, saveBotObj) => {
-	await botlist.editBot(
-		botName,
-		saveBotObj.filepath,
-		saveBotObj.headers,
-		saveBotObj.status,
-		saveBotObj.botIteration,
-		saveBotObj.variables
-	);
-});
+  ipcMain.handle("bots", async (event) => {
+    const result = await botlist.listAllBots();
+    return result;
+  });
+}
 
-ipcMain.handle("bots", async (event) => {
-	const result = await botlist.listAllBots();
-	return result;
-});
+module.exports = { botHandler };
--- backend/app/main.js
+++ backend/app/main.js
@@ -1,33 +1,31 @@
 const {
-	app,
-	Menu,
-	ipcMain,
-	dialog,
-	Notification,
-	BrowserWindow,
+  app,
+  Menu,
+  ipcMain,
+  dialog,
+  Notification,
+  BrowserWindow,
 } = require("electron");
 
 let mainWindow = null;
 
 app.on("ready", () => {
-	mainWindow = require("./WindowManagement/windowConfig").mainWindow;
-	mainWindow.webContents.openDevTools();
+  mainWindow = require("./WindowManagement/windowConfig").mainWindow;
+  mainWindow.webContents.openDevTools();
 });
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-	//macos
-	if (mainWindow === null) {
-		mainWindow = require("./WindowManagement/windowConfig").mainWindow;
-	}
+  //macos
+  if (mainWindow === null) {
+    mainWindow = require("./WindowManagement/windowConfig").mainWindow;
+  }
 });
-
-require("./Bots/bots");
 
 //** Menu building section START */
 //require Templete
@@ -39,3 +37,13 @@ Menu.setApplicationMenu(mainMenu);
 //menu dev/prod configuration
 require("./ActionBar/config");
 //** Menu building section END */
+
+// Import Here
+const menuConfiguration = require("./ActionBar/config");
+const bots = require("./Bots/bots");
+const importExport = require("./ExternalConnectivity/ImportExport");
+
+// Connect Here
+menuConfiguration.config();
+bots.botHandler();
+importExport.importExportHandler();
