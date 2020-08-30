4b8356b3bc110551ac380d817777ef626c83af87
Shanewas Ahmed <shanewasahmed@gmail.com>
2020-08-30 18:45:13 +0600

    code generation fixed || upload - download for inside bot module ready || need finalizing after frontend done

--- backend/app/ExternalConnectivity/codeGeneration.js
+++ backend/app/ExternalConnectivity/codeGeneration.js
@@ -1,18 +1,22 @@
 const { ipcMain, dialog } = require("electron");
 const fs = require("fs");
 
-ipcMain.on("code-generation", async (event, file) => {
-	let options = {
-		title: "Save Generated Python File",
-		buttonLabel: "Save Script",
-		filters: [
-			{ name: "Python", extensions: ["py"] },
-			{ name: "All Files", extensions: ["*"] },
-		],
-	};
-	const save = dialog.showSaveDialog(win, options);
-	fs.writeFile((await save).filePath, file, function (err) {
-		if (err) console.log("Canceled!");
-		else console.log("Saved!");
-	});
-});
+function code_generation(mainWindow) {
+  ipcMain.on("code-generation", async (event, file) => {
+    let options = {
+      title: "Save Generated Python File",
+      buttonLabel: "Save Script",
+      filters: [
+        { name: "Python", extensions: ["py"] },
+        { name: "All Files", extensions: ["*"] },
+      ],
+    };
+    const save = dialog.showSaveDialog(mainWindow, options);
+    fs.writeFile((await save).filePath, file, function (err) {
+      if (err) console.log("Canceled!");
+      else console.log("Saved!");
+    });
+  });
+}
+
+module.exports = { code_generation };
