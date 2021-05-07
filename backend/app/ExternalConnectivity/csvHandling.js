3c7cf819ed6b9d776c6653206299cd9c6b5633d3
Shanewas Ahmed <shanewasahmed@gmail.com>
2021-05-07 20:55:55 +0600

    row line return

--- backend/app/ExternalConnectivity/csvHandling.js
+++ backend/app/ExternalConnectivity/csvHandling.js
@@ -4,29 +4,42 @@ const csv = require("fast-csv");
 const { ipcMain } = require("electron");
 
 const maxLineLength = 100;
-ipcMain.on(
-  "csv-get-header",
-  async (event, file, readPartOfFileMultiplier = 2) => {
-    header = [];
-    var readPartOfFile = maxLineLength * readPartOfFileMultiplier;
-    var stream = fs.createReadStream(path.resolve(__dirname, file), {
-      start: 0,
-      end: readPartOfFile,
-    });
-    var csvStream = csv.parseStream(stream);
-    var line = 0;
-    csvStream.on("data", function (data) {
-      if (line === 0) {
-        header = data;
-      }
-      line++;
-    });
-    csvStream.on("end", function () {
-      console.log("Reading for first line: " + line + " lines.");
-      event.returnValue = header;
-    });
-  }
-);
+// ipcMain.on(
+//   "csv-get-header",
+//   async (event, file, readPartOfFileMultiplier = 2) => {
+//     header = [];
+//     var readPartOfFile = maxLineLength * readPartOfFileMultiplier;
+//     var stream = fs.createReadStream(path.resolve(__dirname, file), {
+//       start: 0,
+//       end: readPartOfFile,
+//     });
+//     var csvStream = csv.parseStream(stream);
+//     var line = 0;
+//     csvStream.on("data", function (data) {
+//       if (line === 0) {
+//         header = data;
+//       }
+//       line++;
+//     });
+//     csvStream.on("end", function () {
+//       console.log("Reading for first line: " + line + " lines.");
+//       event.returnValue = header;
+//     });
+//   }
+// );
+ipcMain.on("csv-get-row", async (event, file) => {
+  var readPartOfFile = maxLineLength * 2;
+  var stream = fs.createReadStream(path.resolve(__dirname));
+  var csvStream = csv.parseStream(stream);
+  var line = 0;
+  csvStream.on("data", function (data) {
+    line++;
+  });
+  csvStream.on("end", function () {
+    console.log("Reading for first line: " + line + " lines.");
+    event.returnValue = line;
+  });
+});
 
 // const handleLoadCsv = (file) => {
 //   Papa.parse(file, {
